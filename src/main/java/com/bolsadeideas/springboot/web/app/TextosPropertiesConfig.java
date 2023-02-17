6fe84422ae85ab6ca6550329a03612f3f8e35502
Alvarogtz <alvaro.zabala@hotmail.es>
2023-02-17 17:06:59 +0000

    Proyecto 1 terminado

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/TextosPropertiesConfig.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.web.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:messages.properties")
+})
+public class TextosPropertiesConfig {
+
+}
---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "redirect:https://www.google.com";
+		//return "forward:/app/index";
+	}
+}
